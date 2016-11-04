d8fb01313d88cda7dfc4eb19c5633514857706f8
Douglas Alves <alves.douglaz@gmail.com>
2016-11-04 12:34:48 -0200

    Created constants paths

--- bootstrap.php
+++ bootstrap.php
@@ -1,3 +1,8 @@
 <?php
-$autoload = require_once(dirname(__FILE__) . "/vendor/autoload.php");
-require_once(dirname(__FILE__) . "/includes/functions.php");
+define('BOOTSTRAP_PATH', dirname(__FILE__));
+define('INCLUDES_PATH', dirname(__FILE__)) . '/includes';
+define('EXEMPLE_URL', '/exemplos/');
+define('ASSETS_URL', '/assets/');
+
+$autoload = require_once(BOOTSTRAP_PATH . "/vendor/autoload.php");
+require_once(INCLUDES_PATH . "functions.php");
