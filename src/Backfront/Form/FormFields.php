8da802456ee906410da75273906ee823082b4876
Douglas Alves <alves.douglaz@gmail.com>
2016-11-07 14:35:10 -0200

    created method 'is_selected' - responsible to checking the checkbox, radio button and select field

--- src/Backfront/Form/FormFields.php
+++ src/Backfront/Form/FormFields.php
@@ -42,98 +42,124 @@
 namespace Backfront\Form
 {
 
-    abstract class FormFields
+    abstract class FormFields implements IFormFields
     {
 
         public function __call($name, $arguments)
         {
             trigger_error("O field de tipo <i>{$name}</i> não pode ser gerado", E_USER_WARNING);
         }
-        
+
         public static function text($args)
         {
             return "olá mundo";
         }
-        
+
         public static function textarea($args)
         {
             
         }
-        
+
         public static function number($args)
         {
             
         }
-        
+
         public static function checkbox($args)
         {
             
         }
-        
+
         public static function radio($args)
         {
             
         }
-        
+
         public static function select($args)
         {
             
         }
         
-        public static function upload_media($args)
-        {
-            
-        }
-        
-        public static function upload_image($args)
-        {
-            
-        }
-        
-        public static function submit($args)
-        {
-            
-        }
-        
-        public static function button($args)
-        {
-            
-        }
-        
-        public static function title($args)
-        {
-            
-        }
-        
-        public static function separator($args)
-        {
-            
-        }
-        
-        public static function url($args)
-        {
-            
-        }
-        
-        public static function color_picker($args)
-        {
-            
-        }
-        
-        public static function icon_picker($args)
-        {
-            
-        }
-        
-        public static function date($args)
-        {
-            
-        }
-        
-        public static function custom($args)
+        public static function file_input($args)
         {
             
         }
 
+        public static function upload_media($args)
+        {
+            
+        }
+
+        public static function upload_image($args)
+        {
+            
+        }
+
+        public static function submit($args)
+        {
+            
+        }
+
+        public static function button($args)
+        {
+            
+        }
+
+        public static function title($args)
+        {
+            
+        }
+
+        public static function separator($args)
+        {
+            
+        }
+
+        public static function url($args)
+        {
+            
+        }
+
+        public static function color_picker($args)
+        {
+            
+        }
+
+        public static function icon_picker($args)
+        {
+            
+        }
+
+        public static function date($args)
+        {
+            
+        }
+
+        public static function custom($args)
+        {
+            
+        }
+        
+        
+        /**
+         * <h3>is_selected</h3>
+         * 
+         * Verify if the current value is selected
+         * <small>use the database value in variable $selected</small>
+         * 
+         * @param string $current
+         * @param string $selected
+         * @param string $type
+         * @return string
+         */
+        public static function is_selected($current, $selected, $type = "select")
+        {
+            $s = null;
+            if ($current == $selected && $type == 'select')
+                return "selected=\"selected\" ";
+            if ($current == $selected && $type == 'check')
+                return "checked=\"checked\" ";
+            return;
+        }
     }
 }
