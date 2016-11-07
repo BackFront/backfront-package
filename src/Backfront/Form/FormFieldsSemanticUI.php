1097fef6dbe37e65b14c2ffe6dedfd46f3e2d51d
Douglas Alves <alves.douglaz@gmail.com>
2016-11-07 14:36:46 -0200

    Create a abstract class 'FormFieldSemanticUI' - responsible to generate html fields using the frontend frameword 'Semanti-ui'

---
+++ src/Backfront/Form/FormFieldsSemanticUI.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * <h1>FormFieldsSemanticUI</h1>
+ * 
+ * Project Name: Backfront
+ * Project URI: https://github.com/BackFront/umbrella-packege
+ * Description: Generate the HTML fields
+ * Version: 1.0.0
+ * Author: Douglas Alves
+ * Author URI: http://alvesdouglas.com.br/
+ * License: Apache License 2.0
+ * 
+ * @package Backfront
+ * @subpackage Form
+ * @version 1.0.0
+ * 
+ * @author Douglas Alves <alves.douglaz@gmail.com>
+ * @link https://github.com/BackFront/backfront-package Project Repository
+ * @license http://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
+ * @since 1.0.0
+ * 
+ * FIELDS:
+ * - text
+ * - textarea
+ * - number
+ * - checkbox
+ * - radio
+ * - select
+ * - file input/media
+ * - image input
+ * - submit
+ * - button
+ * - title
+ * - separator
+ * - url
+ * - color picker
+ * - icon_picker
+ * - date
+ * - custom
+ */
+
+namespace Backfront\Form
+{
+
+    class FormFieldsSemanticUI implements IFormFields
+    {
+
+        public function __call($name, $arguments)
+        {
+            trigger_error("O field de tipo <i>{$name}</i> não pode ser gerado", E_USER_WARNING);
+        }
+
+        public static function text($args)
+        {
+            return "olá mundo UI <br>";
+        }
+
+        public static function textarea($args)
+        {
+            
+        }
+
+        public static function checkbox($args)
+        {
+            
+        }
+
+        public static function radio($args)
+        {
+            
+        }
+
+        public static function select($args)
+        {
+            
+        }
+        
+        public static function file_input($args)
+        {
+            
+        }
+
+        public static function submit($args)
+        {
+            
+        }
+
+        public static function button($args)
+        {
+            
+        }
+
+        /**
+         * <h3>is_selected</h3>
+         * 
+         * Verify if the current value is selected
+         * <small>use the database value in variable $selected</small>
+         * 
+         * @param string $current
+         * @param string $selected
+         * @param string $type
+         * @return string
+         */
+        public static function is_selected($current, $selected, $type = "select")
+        {
+            $s = null;
+            if ($current == $selected && $type == 'select')
+                return "selected=\"selected\" ";
+            if ($current == $selected && $type == 'check')
+                return "checked=\"checked\" ";
+            return;
+        }
+
+    }
+}
