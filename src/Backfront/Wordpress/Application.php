80de4e297366ba47e2bd9be748bbcc9e3513f0d3
Douglas Alves <alves.douglaz@gmail.com>
2017-05-05 16:18:13 -0300

    init class application for wordpress

---
+++ src/Backfront/Wordpress/Application.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * <h1>Application</h1>
+ * 
+ * Project Name: Backfront
+ * Project URI: https://github.com/BackFront/umbrella-packege
+ * Description: Class of controller in wordpress MVC
+ * Version: 1.0.0
+ * Author: Douglas Alves
+ * Author URI: http://alvesdouglas.com.br/
+ * License: Apache License 2.0
+ * 
+ * @package Backfront
+ * @subpackage Wordpress
+ * @version 1.0.0
+ * 
+ * @author Douglas Alves <alves.douglaz@gmail.com>
+ * @link https://github.com/BackFront/backfront-package Project Repository
+ * @license http://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
+ * @since 1.0.0
+ */
+
+namespace Wordpress {
+
+    class Application
+    {
+        
+    }
+}
