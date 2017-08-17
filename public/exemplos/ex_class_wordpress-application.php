f178d79ce9c10194a4a8afded76e8b75ce02610b
Douglas Alves <alves.douglaz@gmail.com>
2017-08-17 14:39:28 -0300

    update composer dependences

---
+++ public/exemplos/ex_class_wordpress-application.php
@@ -0,0 +1,21 @@
+<?php
+require_once (dirname(__FILE__) . '/../header.php');
+
+use Backfront\Wordpress\Application;
+?>
+
+<div class="row">
+    <div class="col-md-12">
+        <div class="panel panel-default">
+            <h3 class="panel-heading">
+                <b>Wordpress/Application</b>
+            </h3>
+            <div class="panel-body">
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+require_once (dirname(__FILE__) . '/../footer.php');
