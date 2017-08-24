3c132f51a1e975a8687ef7cd68ddf4ae7d4b7029
Douglas Alves <alves.douglaz@gmail.com>
2017-08-24 19:31:28 -0300

    required wp version in Wordpress/Application   _contruct()

--- src/Backfront/Wordpress/Application.php
+++ src/Backfront/Wordpress/Application.php
@@ -5,27 +5,39 @@
  * Project Name: Backfront
  * Project URI: https://github.com/BackFront/umbrella-packege
  * Description: Class of controller in wordpress MVC
- * Version: 1.0.0
+ * Version: 1.0
  * Author: Douglas Alves
  * Author URI: http://alvesdouglas.com.br/
  * License: Apache License 2.0
  * 
  * @package Backfront
  * @subpackage Wordpress
- * @version 1.0.0
+ * @version 1.0
  * 
  * @author Douglas Alves <alves.douglaz@gmail.com>
  * @link https://github.com/BackFront/backfront-package Project Repository
  * @license http://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
- * @since 1.0.0
+ * @since 1.0
  */
 
 namespace Backfront\Wordpress
 {
 
-    class Application extends \Backfront\Singleton
+    class Application extends \Backfront\Application
     {
-        
+
+        const WPREQUIRED = '4.8';
+
+        function __construct()
+        {
+            parent::__construct();
+
+            global $wp_version;
+
+            if (version_compare($wp_version, self::WPREQUIRED, '>='))
+                trigger_error("Wordpress required version same or above" . self::WPREQUIRED, E_USER_ERROR);
+        }
+
     }
 
 }
