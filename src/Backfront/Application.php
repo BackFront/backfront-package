3e0a29cfc61c35146ea7c0706743e1b77ea727fb
Douglas Alves <alves.douglaz@gmail.com>
2018-02-28 15:33:30 -0300

    checkouting Class Application

--- src/Backfront/Application.php
+++ src/Backfront/Application.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * <h1>Application</h1>
- * 
+ *
  * Project Name: Backfront
  * Project URI: https://github.com/BackFront/umbrella-packege
  * Description: Run the main methods of package (CORE)
@@ -9,19 +9,18 @@
  * Author: Douglas Alves
  * Author URI: http://alvesdouglas.com.br/
  * License: Apache License 2.0
- * 
+ *
  * @package Backfront
  * @subpackage Form
  * @version 1.0.0
- * 
+ *
  * @author Douglas Alves <alves.douglaz@gmail.com>
  * @link https://github.com/BackFront/backfront-package Project Repository
  * @license http://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
  * @since 1.0.0
  */
 
-namespace Backfront
-{
+namespace Backfront {
 
     use Twig_Loader_Filesystem;
     use Twig_Environment;
@@ -46,14 +45,14 @@ namespace Backfront
 
         /**
          * This method is responsible to register and call a module
-         * 
+         *
          * Obs: Before registers modules, is necessary set '$MDLPATH'
-         * 
+         *
          * @param string $moduleName
          */
         public function registerModule($moduleName)
         {
-            if (is_null(self::getInstance()->MDLPATH)):
+            if(is_null(self::getInstance()->MDLPATH)):
                 trigger_error("It is not possible make calls to the module. It is necessary to specify the path to modules directory", E_USER_NOTICE);
             else:
                 echo $moduleName;
@@ -62,15 +61,15 @@ namespace Backfront
 
         /**
          * This method is responsible to create and return an instance of the twig
-         * 
+         *
          * Obs: Before is necessary set the '$TPLPATH'
-         * 
+         *
          */
         public static function twig()
         {
-            if (is_null(self::getInstance()->twig)) {
-                if (is_null(self::getInstance()->TPLPATH)):
-                    trigger_error("It is not possible load twig. It is necessary to specify the path to templates directory", E_USER_NOTICE);
+            if(is_null(self::getInstance()->twig)) {
+                if(is_null(self::getInstance()->TPLPATH)):
+                    trigger_error("It is not possible load twig cause is necessary to specify the path to templates directory before", E_USER_NOTICE);
                     return self::getInstance();
                 else:
                     $twigLoader = new Twig_Loader_Filesystem(self::getInstance()->TPLPATH);
@@ -82,12 +81,18 @@ namespace Backfront
 
         public static function dirname($path, $levels = 1, $inc = null)
         {
-            if ($levels > 1) {
+            if($levels > 1) {
                 return dirname(self::dirname($path, --$levels)) . $inc;
             } else {
                 return dirname($path) . $inc;
             }
         }
 
+        public static function container()
+        {
+            if(is_null(self::getInstance()->container))
+                return self::getInstance()->container = new Container;
+            return self::getInstance()->contianer;
+        }
     }
 }
